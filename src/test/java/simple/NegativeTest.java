c083d2f3f08f1f899addaf41831c3a4a53e85982
vladislavkadyrov <vlad-kad00@mail.ru>
2023-02-12 16:16:02 +0500

    Add Test

---
+++ src/test/java/simple/NegativeTest.java
@@ -0,0 +1,24 @@
+package simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@Tag("simple")
+public class NegativeTest {
+
+    @Test
+    void test1 () {
+        assertTrue(false);
+    }
+
+    @Test
+    void test2 () {
+        assertTrue(false);
+    }
+
+    @Test
+    void test3 () {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/simple/SkippedTest.java
@@ -0,0 +1,34 @@
+package simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@Tag("simple")
+public class SkippedTest {
+
+    @Test
+    @Disabled("Some reason")
+    void test1 () {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    void test2 () {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    void test3 () {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    void test4 () {
+        assertTrue(true);
+    }
+}
